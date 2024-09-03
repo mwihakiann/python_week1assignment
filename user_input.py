bf98a7e08b99b07e8d3891213a05a4bbbfaa954f
MaryAnn <mwihakiann902@gmail.com>
2024-09-03 16:17:58 +0300

    Responses to week 1 python.

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
